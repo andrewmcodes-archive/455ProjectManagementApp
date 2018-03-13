ba0febcb24773c672fdff3cb498be83334ce081e
Andrew Mason <astrosurfer888@gmail.com>
2018-03-13 19:42:35 -0400

    Added Sentry Integration

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,9 @@ module CRDCSaasApp
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    Raven.configure do |config|
+      config.dsn = ENV['SENTRY_DSN']
+      config.attr = 'value'
+    end
   end
 end
