56cbc606cbaf441f34415cd05383e5a605fce394
andrewmcodes <am5648@uncw.edu>
2018-04-15 23:58:49 -0400

    continue rails 5.2 upgrade

--- config/boot.rb
+++ config/boot.rb
@@ -1,5 +1,4 @@
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __dir__)
-# Set up gems listed in the Gemfile.
-require 'bundler/setup'
-# Require for bootsnap gem
-require 'bootsnap/setup'
+
+require 'bundler/setup' # Set up gems listed in the Gemfile.
+require 'bootsnap/setup' # Speed up boot time by caching expensive operations.
